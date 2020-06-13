c5b3abcb6ff08f366fa839a4cfa1380e698e3f66
tober65 <timothy.ober@gmail.com>
2020-06-13 13:25:47 -0700

    Implemented a timer

---
+++ script.js
@@ -0,0 +1,17 @@
+timeEl = document.getElementById("timer");
+
+var timeLeft = 5;
+
+function startTimer() {
+    var timerInterval = setInterval(function () {
+        timeLeft--;
+        timeEl.textContent = "Time: " + timeLeft;
+
+        if (timeLeft <= 0) {
+            timeLeft = 0;
+            timeEl.textContent = "Time: " + timeLeft;
+
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
