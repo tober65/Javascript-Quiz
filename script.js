97a38575a26299cc50c7d6f120c1f48e1f01f80a
tober65 <timothy.ober@gmail.com>
2020-06-15 14:35:35 -0700

    Added questions

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-question1 = {
-    question: "Commonly used data types do not include:",
-    answers: ["Strings", "Booleans", "Alerts", "Numbers"],
-    answerIndex: 2
-}
+questions = [
+    {
+        question: "Commonly used data types do not include:",
+        answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answerIndex: 2
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within:",
+        answers: ["Quote", "Curly Braces", "Parentheses", "Square Brackets"],
+        answerIndex: 1
+    },
+    {
+        question: "Arrays in Javascript can be used to store _____.",
+        answers: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answerIndex: 3
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answers: ["Commas", "Curly Braces", "Quotes", "Parentheses"],
+        answerIndex: 2
+    },
+    {
+        question: "A very useful tool used when developing and debugging for printing content to the debugger is:",
+        answers: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answerIndex: 3
+    }
+]
 
 timeEl = document.getElementById("timer");
 codingStartEl = document.getElementById("codingStartDiv");
@@ -16,6 +38,7 @@ var answer3El = document.getElementById("button3");
 
 var timeLeft = 5;
 var correctAnswer = -1;
+questionIndex = 0;
 
 function displayQuestion(question) {
     questionEl.textContent = question.question;
@@ -32,7 +55,7 @@ function startTimer() {
     quizEl.classList.remove("hide");
     timeEl.textContent = "Time: " + timeLeft;
 
-    displayQuestion(question1);
+    displayQuestion(questions[questionIndex]);
 
     var timerInterval = setInterval(function () {
         timeLeft--;
@@ -52,27 +75,39 @@ function goToHomepage() {
 }
 
 function checkForRightAnswer(answer) {
-    if(answer === correctAnswer) {
+    if (answer === correctAnswer) {
         console.log("Right answer");
     }
     else {
         console.log("Wrong answer");
         timeLeft -= 10;
     }
+
+    questionIndex++;
+    if (questionIndex === questions.length) {
+        stopQuiz();
+    }
+    else {
+        displayQuestion(questions[questionIndex]);
+    }
 }
 
-answer0El.addEventListener("click", function() {
+function stopQuiz() {
+    //TODO
+}
+
+answer0El.addEventListener("click", function () {
     checkForRightAnswer(0);
 });
 
-answer1El.addEventListener("click", function() {
+answer1El.addEventListener("click", function () {
     checkForRightAnswer(1);
 });
 
-answer2El.addEventListener("click", function() {
+answer2El.addEventListener("click", function () {
     checkForRightAnswer(2);
 });
 
-answer3El.addEventListener("click", function() {
+answer3El.addEventListener("click", function () {
     checkForRightAnswer(3);
 });
